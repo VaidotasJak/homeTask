9586368e1e623549a5e7085999a7238d2a5d3dac
vaidotas.jakavicius <vaidotas.jakavicius@VAIDOTASJAKAVICIUS.x.insoft.lt>
2019-11-12 15:35:26 +0200

    run with Junit5

--- automationFrame/src/test/java/com/automationframe/Runner/testRunner.java
+++ automationFrame/src/test/java/com/automationframe/Runner/testRunner.java
@@ -1,15 +1,9 @@
 package com.automationframe.Runner;
 
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-
-import com.automationframe.test.Scripts;
-
 public class testRunner {
 	
 		public static void main(String[] args) {
-			JUnitCore junit = new JUnitCore();
-			Result result = junit.run(Scripts.class);
-			System.out.println(result.getFailureCount());
+			RunJUnit5TestsFromJava runner = new RunJUnit5TestsFromJava();
+		    runner.runOne();
 	}
 }
